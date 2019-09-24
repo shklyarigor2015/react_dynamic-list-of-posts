80e0cfd33d10489386cc3cb3b0d7deca8d582655
unknown <shklyarigor2015@gmail.com>
2019-09-24 10:36:27 +0300

    Updates

---
+++ precache-manifest.45dcc4dd9ba9f5e84f8c86e708788e14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "646f6797bd1cdc441b90d1bdde7c250d",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "fc3fa19b98b8d7786235",
+    "url": "/react_dynamic-list-of-posts/static/css/main.bfbd1e88.chunk.css"
+  },
+  {
+    "revision": "3548081dfc8200ae1b7b",
+    "url": "/react_dynamic-list-of-posts/static/js/2.d04b2959.chunk.js"
+  },
+  {
+    "revision": "fc3fa19b98b8d7786235",
+    "url": "/react_dynamic-list-of-posts/static/js/main.dcea40e7.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  }
+]);
